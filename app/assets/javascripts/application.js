6534c77b941bdbd45aa3c012b0bf921d733832a3
peterhoejlundandersen <hilsenit@gmail.com>
2017-11-30 17:54:30 +0100

    perfect showing of search field

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,6 @@
 //= require rails-ujs
 //= require_tree .
 
-
-// LAYOUT JS
 toggleHeight = function(menu_wrapper_selector) {
   var menu_wrapper = document.querySelector(menu_wrapper_selector);
   if ( menu_wrapper.classList.contains('menu-wrapper-open') ) {
@@ -26,23 +24,25 @@ toggleHeight = function(menu_wrapper_selector) {
 
 openOrHide = function() {
   var search_page = document.querySelector('[data-search="page"]');
+  var button = document.getElementById("searchButton");
   if (search_page.classList.contains('hide-it')) {
     search_page.classList.remove('hide-it');
     document.querySelector('[data-search="input"]').focus();
+    button.classList.add('active');
   } else {
     search_page.classList.add('hide-it');
+    button.classList.remove('active');
   }
 };
+
 window.onload = function() {
   document.querySelector('[data-toggle="menu"]').addEventListener('click', function() {
     toggleHeight('[data-slide="open"]');
   });
-  // Search page
+  // Search page show/hide
   document.querySelectorAll('[data-search="openOrHide"]').forEach(function(item) {
     item.addEventListener('click', function() {
       openOrHide();
     });
   });
 };
-
-// LAYOUT JS
