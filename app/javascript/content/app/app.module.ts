ac3a99f05371b5cf41027e905c49c5497174ebe5
peterhoejlundandersen <hilsenit@gmail.com>
2017-12-09 16:12:33 +0100

    angular set up on work_index for the difficult view - excited

--- app/javascript/content/app/app.module.ts
+++ app/javascript/content/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { ContentComponent } from './content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+   ContentComponent 
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ContentComponent]
 })
 export class AppModule { }
---
+++ app/javascript/content/app/content.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'content',
+  template: `Så er vi igang!`
+})
+export class ContentComponent {
+  name = 'Angular!';
+}
